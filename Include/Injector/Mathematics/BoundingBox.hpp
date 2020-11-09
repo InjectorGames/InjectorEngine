a0f99eda3ebd10dab7849f5864105cd8ac5647c5
Nikita Fediuchin <cfnptr@gmail.com>
2020-11-09 21:23:19 +0200

    Bounding box, sphere

--- Include/Injector/Mathematics/BoundingBox.hpp
+++ Include/Injector/Mathematics/BoundingBox.hpp
@@ -5,16 +5,26 @@ namespace Injector
 {
 	struct BoundingBox
 	{
-		Vector3 position;
-		Vector3 size;
+		Vector3 minimum;
+		Vector3 maximum;
 
 		BoundingBox(
-			const Vector3& position,
-			const Vector3& size);
+			const Vector3& minimum,
+			const Vector3& maximum) noexcept;
+
+		void setCenterAndSize(
+			const Vector3& center,
+			const Vector3& size) noexcept;
+		void setCenterAndExtent(
+			const Vector3& center,
+			const Vector3& extent) noexcept;
+
+		void resize(
+			const Vector3& offset) noexcept;
 
 		bool isColliding(
-			const BoundingBox& box) noexcept;
-		bool isColliding(
 			const Vector3& point) noexcept;
+		bool isColliding(
+			const BoundingBox& box) noexcept;
 	};
 }
--- Source/Mathematics/BoundingBox.cpp
+++ Source/Mathematics/BoundingBox.cpp
@@ -3,28 +3,50 @@
 namespace Injector
 {
 	BoundingBox::BoundingBox(
-		const Vector3& _position,
-		const Vector3& _size) :
-		position(_position),
-		size(_size)
+		const Vector3& _minimum,
+		const Vector3& _maximum) noexcept :
+		minimum(_minimum),
+		maximum(_maximum)
 	{
 	}
 
+	void BoundingBox::setCenterAndSize(
+		const Vector3& center,
+		const Vector3& size) noexcept
+	{
+		auto half = size / 2.0f;
+		minimum = center - half;
+		maximum = center + half;
+	}
+	void BoundingBox::setCenterAndExtent(
+		const Vector3& center,
+		const Vector3& extent) noexcept
+	{
+		minimum = center - extent;
+		maximum = center + extent;
+	}
+
+	void BoundingBox::resize(
+		const Vector3& offset) noexcept
+	{
+		minimum -= offset;
+		maximum += offset;
+	}
+
+	bool BoundingBox::isColliding(
+		const Vector3& point) noexcept
+	{
+		return
+			minimum.x <= point.x && maximum.x >= point.x &&
+			minimum.y <= point.y && maximum.y >= point.y &&
+			minimum.z <= point.z && maximum.z >= point.z;
+	}
 	bool BoundingBox::isColliding(
 		const BoundingBox& box) noexcept
 	{
 		return
-			position.x < box.position.x + box.size.x &&
-			position.x + size.x > box.position.x &&
-			position.y < box.position.y + box.size.y &&
-			position.y + size.y > box.position.y &&
-			position.z < box.position.z + box.size.z &&
-			position.z + size.z > box.position.y;
-	}
-	bool BoundingBox::isColliding(
-		const Vector3& point) noexcept
-	{
-		// TODO:
-		return true;
+			minimum.x <= box.maximum.x && maximum.x >= box.minimum.x &&
+			minimum.y <= box.maximum.y && maximum.y >= box.minimum.y &&
+			minimum.z <= box.maximum.z && maximum.z >= box.minimum.z;
 	}
 }
